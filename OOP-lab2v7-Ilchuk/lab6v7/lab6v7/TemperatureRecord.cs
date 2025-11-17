2dab4075f6682bc2756f6e2c3f814799d82bd55d
denusilchuk9-cell <denusilchuk9@gmail.com>
2025-11-17 17:46:53 +0200

    Add files via upload

---
+++ OOP-lab2v7-Ilchuk/lab6v7/lab6v7/TemperatureRecord.cs
@@ -0,0 +1,13 @@
+﻿public class TemperatureRecord
+{
+    public string Day { get; set; }
+    public double Celsius { get; set; }
+
+    public TemperatureRecord(string day, double celsius)
+    {
+        Day = day;
+        Celsius = celsius;
+    }
+}
+
+}
