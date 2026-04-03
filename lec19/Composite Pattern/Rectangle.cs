cc4ed25a4f53545dc11c6156a81d6c3c434bcb24
denusilchuk9-cell <denusilchuk9@gmail.com>
2026-04-03 20:48:39 +0300

    Add files via upload

---
+++ lec19/Composite Pattern/Rectangle.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CompositeDecoratorDemo.Composite
+{
+    public class Rectangle : IGraphic
+    {
+        public void Draw()
+        {
+            Console.WriteLine("Drawing a Rectangle");
+        }
+    }
+}
---
+++ lec19/Decorator Pattern/CoffeeDecorator.cs
@@ -0,0 +1,22 @@
+﻿namespace CompositeDecoratorDemo.Decorator
+{
+    public abstract class CoffeeDecorator : ICoffee
+    {
+        protected ICoffee _decoratedCoffee;
+
+        public CoffeeDecorator(ICoffee coffee)
+        {
+            _decoratedCoffee = coffee;
+        }
+
+        public virtual string GetDescription()
+        {
+            return _decoratedCoffee.GetDescription();
+        }
+
+        public virtual double GetCost()
+        {
+            return _decoratedCoffee.GetCost();
+        }
+    }
+}
